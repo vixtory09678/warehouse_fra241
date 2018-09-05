3664d346791b6407b051e7344877cde157bfd609
roboticsza <robotics.za@gmail.com>
2018-09-05 16:30:35 +0700

    add retieveItem method & wait for write code in body

--- src/data/Warehouse.java
+++ src/data/Warehouse.java
@@ -3,32 +3,49 @@ package data;
 import java.util.*;
 import model.ProductItem;
 
-@SuppressWarnings("unchecked")
 public class Warehouse{
 
     private List<HashMap<String,ProductItem>> listItemStore;
     private int maxRows;
+    private int maxGrid;
 
     public Warehouse () {
     }
 
-    public void build(int rows , int grid){
+    // build warehouse
+    public void build(int rows, int grid){
         if(listItemStore.size() > 0) return;
         this.maxRows = rows;
+        this.maxGrid = grid;
 
         listItemStore = new ArrayList<HashMap < String,ProductItem > >();
         setupStock(maxRows);
     }
 
-    public void addItem(int rows, ProductItem item){
-        if(rows > -1 && rows <= maxRows)
-            listItemStore.get(rows).put(item.getId() , item);
+    // add item to stock
+    public String addItem(int rows, ProductItem item){
+        if(rows < 0 || rows > maxRows) return;
+
+        if(listItemStore.get(rows).size() > maxGrid){
+            return MsgProcess.FULL_ERR;
+        }
+        
+        listItemStore.get(rows).put(item.getId() , item);
+
+        return MsgProcess.SUCCESS;
     }
 
+    public String retieveItem(int rows, ProductItem item){
+
+    }
+
+    // check item available 
     public boolean isItem(int rows, ProductItem item){
+        // if(rows < 0 || rows > maxRows) return;
         return listItemStore.get(rows).containsKey(item.getId());
     }
 
+    // setup stock
     private void setupStock(int rows){
         for (int i = 0; i < rows; i++) {
             listItemStore.add(new HashMap<String,ProductItem>());
