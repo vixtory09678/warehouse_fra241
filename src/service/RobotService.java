aa858ea21a985072c8e7a9269b711ee0573859ff
roboticsza <robotics.za@gmail.com>
2018-09-05 16:33:08 +0700

    update store & pick method

--- src/service/RobotService.java
+++ src/service/RobotService.java
@@ -1,29 +1,45 @@
 package service;
 public class RobotService extends BaseClass{
 
+    private final int WAREHOUSE_COUNT = 5;
+
     ConveyorBelt belt;
-    Warehouse w1;
-    Warehouse w2;
-    Warehouse w3;
-    Warehouse w4;
-    Warehouse w5;
+    Warehouse[] warehouse;
 
     public RobotService(){
         belt = new ConveyorBelt();
-        w1 = new Warehouse();
-        w2 = new Warehouse();
-        w3 = new Warehouse();
-        w4 = new Warehouse();
-        w5 = new Warehouse();
+        warehouse = new Warehouse[WAREHOUSE_COUNT];
+        warehouse[0].build(5, 10*10);
+        warehouse[1].build(5, 10*10);
+        warehouse[2].build(5, 10*10);
+        warehouse[3].build(7, 5*5);
+        warehouse[4].build(20, 20*20);
     }
 
-    public pick(ProductItem item){
-        if()
+    public String pick(ProductItem item){
+        CodeId warehouseCode = getCodeId(item.getId())
+        if(warehouseCode == null) return;
+
+        int number = findWarehouseNumber(warehouseCode.getKey());
+        
+
         println("Moving from Belt to A");
     }
 
-    public void store(ProductItem item){
+    public String store(ProductItem item){
+        CodeId warehouseCode = getCodeId(item.getId())
+        if(warehouseCode == null) return "";
 
+        int number = findWarehouseNumber(warehouseCode.getKey());
+        warehouse[number].addItem(warehouseCode.getRows(),item);
+    }
+
+    public void sort(int number, int rows){
+        // warehouseCode[number].get(rows).sort()
+    }
+
+    private int findWarehouseNumber(Char key){
+        return (key - 'A') % WAREHOUSE_COUNT;
     }
 
 }
